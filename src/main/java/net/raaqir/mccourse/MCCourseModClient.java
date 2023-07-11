b34396afe94600bdb4d66403106e81170a566e19
Navin Antoine <freenavin@yahoo.com>
2023-07-11 12:03:57 -0400

    Initial commit

---
+++ src/main/java/net/raaqir/mccourse/MCCourseModClient.java
@@ -0,0 +1,11 @@
+package net.raaqir.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+
+    @java.lang.Override
+    public void onInitializeClient() {
+
+    }
+}
